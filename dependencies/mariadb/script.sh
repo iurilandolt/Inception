fb61eb2370a395408aba7d23f2e2524a948714a4
iuri- <123178126+iurilandolt@users.noreply.github.com>
2024-10-08 22:04:18 +0100

    configured mariadb?

--- dependencies/mariadb/script.sh
+++ dependencies/mariadb/script.sh
@@ -1,17 +1,35 @@
 #!/bin/bash
 
 
-mysql start 
+# Create necessary directories
+mkdir -p /var/run/mysqld
+mkdir -p /var/log/mysql
+chown -R mysql:mysql /var/run/mysqld
+chown -R mysql:mysql /var/log/mysql
 
+# Initialize the MariaDB data directory if it is empty
+if [ ! -d /var/lib/mysql/mysql ]; then
+  mysql_install_db --user=mysql --datadir=/var/lib/mysql
+fi
 
-echo "CREATE DATABASE IF NOT EXISTS $db_name ;" > db.sql
-echo "CREATE USER IF NOT EXISTS '$db_user'@'%' IDENTIFIED BY '$db_pwd' ;" >> db.sql
-echo "GRANT ALL PRIVILEGES ON $db_name.* TO '$db_user'@'%' ;" >> db.sql
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '12345' ;" >> db.sql
+# Start MariaDB in the background
+mysqld_safe --datadir=/var/lib/mysql --user=mysql &
+
+# Wait for MariaDB to be fully operational
+until mysqladmin ping -h "127.0.0.1" --silent; do
+  echo 'waiting for mysqld to be connectable...'
+  sleep 2
+done
+
+# Create database and user
+echo "CREATE DATABASE IF NOT EXISTS ${db_name} ;" > db.sql
+echo "CREATE USER IF NOT EXISTS '${db_user}'@'%' IDENTIFIED BY '${db_pwd}' ;" >> db.sql
+echo "GRANT ALL PRIVILEGES ON ${db_name}.* TO '${db_user}'@'%' ;" >> db.sql
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}' ;" >> db.sql
 echo "FLUSH PRIVILEGES;" >> db.sql
 
+# Execute the SQL script
 mysql < db.sql
 
-kill $(cat /var/run/mysqld/mysqld.pid)
-
-mysqld
+# Keep the container running
+wait
--- dependencies/nginx/script.sh
+++ dependencies/nginx/script.sh
@@ -2,28 +2,28 @@
 
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt -subj "/C=PT/L=LISBOA/O=42/OU=student/CN=rlandolt"
 
-echo "
-server {
-    listen 443 ssl;
-    listen [::]:443 ssl;
+# echo "
+# server {
+#     listen 443 ssl;
+#     listen [::]:443 ssl;
 
-    #server_name www.\$DOMAIN_NAME \$DOMAIN_NAME;
+#     #server_name www.\$DOMAIN_NAME \$DOMAIN_NAME;
 
-    ssl_certificate /etc/ssl/certs/nginx-selfsigned.crt;
-    ssl_certificate_key /etc/ssl/private/nginx-selfsigned.key;" > /etc/nginx/sites-available/default
+#     ssl_certificate /etc/ssl/certs/nginx-selfsigned.crt;
+#     ssl_certificate_key /etc/ssl/private/nginx-selfsigned.key;" > /etc/nginx/sites-available/default
 
-echo '
-    ssl_protocols TLSv1.3;
+# echo '
+#     ssl_protocols TLSv1.3;
 
-    index index.php;
-    root /var/www/html;
+#     index index.php;
+#     root /var/www/html;
 
-    location ~ [^/]\.php(/|$) { 
-            try_files \$uri =404;
-            fastcgi_pass wordpress:9000;
-            include fastcgi_params;
-            fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
-        }
-} ' >> /etc/nginx/sites-available/default
+#     location ~ [^/]\.php(/|$) { 
+#             try_files \$uri =404;
+#             fastcgi_pass wordpress:9000;
+#             include fastcgi_params;
+#             fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
+#         }
+# } ' >> /etc/nginx/sites-available/default
 
 nginx -g 'daemon off;'
