4eb2d2319dc5c32b5b65130d8527e51f23952a06
iuri- <123178126+iurilandolt@users.noreply.github.com>
2024-10-13 16:41:26 +0100

    basic setup for nginx, wp, and mariadb is working

--- dependencies/mariadb/script.sh
+++ dependencies/mariadb/script.sh
@@ -5,10 +5,11 @@ touch /etc/mysql/init.sql
 
 echo "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE} ;" > /etc/mysql/init.sql
 echo "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}' ;" >> /etc/mysql/init.sql
-echo "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%' ;" >> /etc/mysql/init.sql
+echo "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%' WITH GRANT OPTION;" >> /etc/mysql/init.sql
 echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}' ;" >> /etc/mysql/init.sql
 echo "FLUSH PRIVILEGES;" >> /etc/mysql/init.sql
 
-mysqld < /etc/mysql/init.sql 
+mysql_install_db 
+mysqld #< /etc/mysql/init.sql 
 
 
--- dependencies/wordpress/script.sh
+++ dependencies/wordpress/script.sh
@@ -1,35 +1,26 @@
 #!/bin/bash
-mkdir -p wordpress
-cd wordpress
 
+cd /var/www/html
+
+#rm -rf /var/www/html/*
 
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 
-
-php wp-cli.phar --info
-
-
 chmod +x wp-cli.phar
-
 
 mv wp-cli.phar /usr/local/bin/wp
 
-wp --info
-
-cd ../var/www/html
-
-#rm -rf *
+wp core download --allow-root
 
 
-#wp core download --allow-root
+wp config create --dbname="$WORDPRESS_DB_NAME" --dbuser="$WORDPRESS_DB_USER" --dbpass="$WORDPRESS_DB_PASSWORD" --dbhost="$WORDPRESS_DB_HOST" --allow-root
 
 
-#wp config create --dbname="$WORDPRESS_DB_NAME" --dbuser="$WORDPRESS_DB_USER" --dbpass="$WORDPRESS_DB_PASSWORD" --dbhost="$WORDPRESS_DB_HOST" --allow-root
+wp core install --url=localhost --title="inception" --admin_user=wpcli --admin_password="$MYSQL_ROOT_PASSWORD" --admin_email=rlandolt@student.42lisboa.com --allow-root
 
 
-#wp core install --url=wpclidemo.dev --title="WP-CLI" --admin_user=wpcli --admin_password="$MYSQL_ROOT_PASSWORD" --admin_email=info@wp-cli.org --allow-root
+php-fpm8.2 -F
 
 
-/usr/sbin/php-fpm* -F
 
 
