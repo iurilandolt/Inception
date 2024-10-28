5836cbf0538db9ca9a18fd37f7ce7f363d20ad23
iuri- <123178126+iurilandolt@users.noreply.github.com>
2024-10-28 15:12:54 +0000

    simplified mariadb setup logic

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
 if [ ! -f /etc/mysql/init.sql ]; then
+	
+	# useradd -m -d /var/lib/mysql $(cat /run/secrets/wpmngr)
+	# echo ""$(cat /run/secrets/wpmngr)":"$(cat /run/secrets/dbpsw)"" | chpasswd
+	# sed -i 's/user                    = mysql/user                    = '$(cat /run/secrets/wpmngr)/'' /etc/mysql/mariadb.conf.d/50-server.cnf
+	# chown -R $(cat /run/secrets/wpmngr):$(cat /run/secrets/wpmngr) /etc/mysql /var/lib/mysql /run/mysqld
+	# chmod 755 /run/mysqld
+
 	touch /etc/mysql/init.sql
 	echo "CREATE DATABASE IF NOT EXISTS $(cat /run/secrets/dbname) ;" > /etc/mysql/init.sql
 	echo "CREATE USER IF NOT EXISTS '$(cat /run/secrets/dbusr)'@'%' IDENTIFIED BY '$(cat /run/secrets/dbusrpsw)' ;" >> /etc/mysql/init.sql
 	echo "GRANT ALL PRIVILEGES ON $(cat /run/secrets/dbname).* TO '$(cat /run/secrets/dbusr)'@'%' WITH GRANT OPTION;" >> /etc/mysql/init.sql
-	echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$(cat /run/secrets/dbpsw)' ;" >> /etc/mysql/init.sql
+	# echo "ALTER USER '$(cat /run/secrets/wpmngr)'@'localhost' IDENTIFIED BY '$(cat /run/secrets/dbpsw)' ;" >> /etc/mysql/init.sql
+	echo "ALTER USER 'mysql'@'localhost' IDENTIFIED BY '$(cat /run/secrets/dbpsw)' ;" >> /etc/mysql/init.sql
 	echo "FLUSH PRIVILEGES;" >> /etc/mysql/init.sql
 	mysql_install_db
 fi
 
-mysqld 
+# mysqld -u $(cat /run/secrets/wpmngr)
+
+mysqld
 
 
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -2,7 +2,7 @@
 
 if [ ! -f /var/www/html/wp-config.php ]; then
 	wp config create --dbname="$(cat /run/secrets/dbname)" --dbuser="$(cat /run/secrets/wpusr)" --dbpass="$(cat /run/secrets/wpusrpsw)" --dbhost="$(cat /run/secrets/dbhost)" --allow-root
-	wp core install --url=rlandolt.42.fr --title="inception" --admin_user="$(cat /run/secrets/wpmngr)" --admin_password="$(cat /run/secrets/dbpsw)" --admin_email=rlandolt@student.42lisboa.com --allow-root
+	wp core install --url=https://rlandolt.42.fr --title="inception" --admin_user="$(cat /run/secrets/wpmngr)" --admin_password="$(cat /run/secrets/dbpsw)" --admin_email=rlandolt@student.42lisboa.com --allow-root
 	wp user create "$(cat /run/secrets/wpusr)" "$(cat /run/secrets/wpusr)"@"$HOSTNAME".com --role=author --user_pass="$(cat /run/secrets/wpusrpsw)" --allow-root
 	wp theme install skatepark --activate --allow-root
 	wp plugin install redis-cache --activate --allow-root
