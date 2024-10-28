2fd7ee164f78159715a4ccc5a17c09b468cfd720
iuri- <123178126+iurilandolt@users.noreply.github.com>
2024-10-28 17:01:34 +0000

    fixed nginx conf where requests were not properly sent to nodejs, nodejs can now serve multiple files instead of index only, small changes to detail.html page layout

--- srcs/requirements/bonus/nodejs/conf/server.js
+++ srcs/requirements/bonus/nodejs/conf/server.js
@@ -1,15 +1,30 @@
 const http = require('http');
 const fs = require('fs');
+const path = require('path');
 
 const server = http.createServer((req, res) => {
-	fs.readFile('/app/public/index.html', (err, content) => {
+	console.log(`Received request for ${req.url}`);
+
+	let filePath = path.resolve('/app/public' + (req.url === '/' ? '/index.html' : req.url.split('?')[0]));
+	const extname = path.extname(filePath);
+
+	let contentType = 'text/html';
+	// if (extname === '.css') contentType = 'text/css';
+	// else if (extname === '.js') contentType = 'application/javascript';
+
+	fs.readFile(filePath, (err, content) => {
 		if (err) {
-
-			res.writeHead(500, { 'Content-Type': 'text/html' });
-			res.end('Server Error: Unable to load the page');
+			if (err.code === 'ENOENT') {
+				fs.readFile(path.resolve('/app/public/404.html'), (error, content404) => {
+					res.writeHead(404, { 'Content-Type': 'text/html' });
+					res.end(content404 || '404 Nothing to see here', 'utf8');
+				});
+			} else {
+				res.writeHead(500);
+				res.end(`Server Error: ${err.code}`);
+			}
 		} else {
-
-			res.writeHead(200, { 'Content-Type': 'text/html' });
+			res.writeHead(200, { 'Content-Type': contentType });
 			res.end(content, 'utf8');
 		}
 	});
@@ -19,3 +34,4 @@ const PORT = 3000;
 server.listen(PORT, () => {
 	console.log(`Server is running on http://localhost:${PORT}`);
 });
+
